4245930a369744bf3fb24cad31b25c2ee3d0f449
RosarioManny <rosario.emm47@gmail.com>
2024-10-15 15:16:48 -0400

    Created and implemented Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import * as weatherService from './services/weatherService';
-// ^^ This syntax imports everything (*) exported from the weatherService.js module 
-// and groups them inside a weatherService object.
+// ^^ This syntax imports everything (*) exported from the weatherService.js module and groups them inside a weatherService object.
+import WeatherSearch from './components/WeatherSearch';
+import { useState } from 'react'
 
 const App = () => {
+  const [weatherService, setWeather] = useState({})
+
   const fetchData = async () => {
     const data = await weatherService.show('New York');
     console.log('Data:', data);
@@ -11,7 +14,7 @@ const App = () => {
   return (
     <main>
       <h1>Weather API</h1>
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch />
     </main>
   );
 };
---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,34 @@
+// F = Function
+// I = import
+// R = Return 
+// E = Export
+import { useState } from 'react' // I.
+
+const WeatherSearch = (props) => { // F.
+    const [city, setCity] = useState('')
+
+    const handleSubmit = (e) => {
+        // Prevents form default action
+        e.preventDefault();
+        setCity('')
+    };
+
+    return ( // R.
+        <section>
+            <h2>Search</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="city">Enter a city:</label>
+                <input
+                id="city"
+                type="text"
+                name="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+        </section>
+    ) 
+}
+
+export default WeatherSearch // E.
